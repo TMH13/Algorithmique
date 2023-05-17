cc0c785f57545b11a583a1b36b8f75caebb98b59
TMH <copusestan@gmail.com>
2023-05-17 15:04:29 +0200

    add queue functions

--- Structures de données dynamiques/File/file.c
+++ Structures de données dynamiques/File/file.c
@@ -1,5 +1,67 @@
-//
-// Created by TMH on 17/05/2023.
-//
-
 #include "file.h"
+
+int file_enfiler(Node **tete_ptr, Node **queue_ptr, Node *element)
+{
+    Node *new = NULL; // pointeur vers le nouvel élément
+
+    new = (Node *)malloc(sizeof(Node)); // allocation de la mémoire pour le nouvel élément
+
+    if(new == NULL) // si l'allocation a échoué
+        return 0;
+
+    // copie des données de l'élément à enfiler dans le nouvel élément
+    strcpy(new->nom, element->nom);
+    new->age = element->age;
+    new->next = NULL;
+
+    if(file_est_vide(*tete_ptr)) // si la file est vide
+    {
+        *tete_ptr = new; // la tête de la file pointe vers le nouvel élément
+        *queue_ptr = new; // la queue de la file pointe vers le nouvel élément
+    }
+    else
+    {
+        (*queue_ptr)->next = new; // le dernier élément de la file pointe vers le nouvel élément
+        *queue_ptr = new; // la queue de la file pointe vers le nouvel élément
+    }
+
+    return 1; // l'élément a été ajouté à la file
+}
+
+int file_est_vide(Node *tete)
+{
+    return tete == NULL; // retourne 1 si la file est vide, 0 sinon
+}
+
+Node *file_tete_file(Node *tete)
+{
+    if(file_est_vide(tete)) // si la file est vide
+        return NULL;
+
+    return tete; // retourne la tête de la file
+}
+
+Node *file_queue_file(Node *queue)
+{
+    if(file_est_vide(queue)) // si la file est vide
+        return NULL;
+
+    return queue; // retourne la queue de la file
+}
+
+Node *file_defiler(Node **tete_ptr, Node **queue_ptr)
+{
+    Node *tmp = NULL; // pointeur vers l'élément à défiler
+
+    if(file_est_vide(*tete_ptr)) // si la file est vide
+        return NULL;
+
+    tmp = *tete_ptr; // l'élément à défiler est la tête de la file
+    *tete_ptr = (*tete_ptr)->next; // la tête de la file pointe vers l'élément suivant
+
+    if(*tete_ptr == NULL) // si la file est vide
+        *queue_ptr = NULL; // la queue de la file pointe vers NULL
+
+    return tmp; // retourne l'élément à défiler, il peut être ensuite libéré
+}
+
--- Structures de données dynamiques/File/file.h
+++ Structures de données dynamiques/File/file.h
@@ -1,8 +1,80 @@
-//
-// Created by TMH on 17/05/2023.
-//
-
 #ifndef ALGORITHMIQUE_FILE_H
 #define ALGORITHMIQUE_FILE_H
+
+#include "..\..\data.h"
+
+/***********************************************************************************************************************
+
+    On appelle FILE un ensemble formé d’un nombre variable, éventuellement nul, de données, sur lequel on peut
+    effectuer les opérations suivantes :
+        - Ajout d’une nouvelle donnée.
+        - Test déterminant si la file est vide ou non.
+        - Consultation de la première donnée ajoutée et non supprimée depuis (donc la plus ancienne) s’il y en a une.
+        - Suppression de la première donnée ajoutée et non encore supprimée depuis, s’il y en a une.
+
+    Une file doit répondre au critère suivant :
+        "Premier entré, premier sorti" :
+            First In First Out (FIFO
+
+ **********************************************************************************************************************/
+
+
+/***********************************************************************************************************************
+    INPUT:
+        - tete_ptr: pointeur vers la tête de la file
+        - queue_ptr: pointeur vers la queue de la file
+        - element: pointeur vers l'élément à enfiler
+    PROCESS:
+        ajoute un élément à la queue de la file
+    OUTPUT:
+        - 1: si l'élément a été ajouté à la file
+        - 0: si l'élément n'a pas été ajouté à la file
+***********************************************************************************************************************/
+int file_enfiler(Node **tete_ptr, Node **queue_ptr, Node *element);
+
+/***********************************************************************************************************************
+    INPUT:
+        - tete_ptr: pointeur vers la tête de la file
+    PROCESS:
+        teste si la file est vide
+    OUTPUT:
+        - 1: si la file est vide
+        - 0: si la file n'est pas vide
+***********************************************************************************************************************/
+int file_est_vide(Node *tete);
+
+/***********************************************************************************************************************
+    INPUT:
+        - tete_ptr: pointeur vers la tête de la file
+    PROCESS:
+        consulte la tête de la file
+    OUTPUT:
+        - NULL: si la file est vide
+        - Node *: pointeur vers la tête de la file
+***********************************************************************************************************************/
+Node *file_tete_file(Node *tete);
+
+/***********************************************************************************************************************
+    INPUT:
+        - queue_ptr: pointeur vers la queue de la file
+    PROCESS:
+        consulte la queue de la file
+    OUTPUT:
+        - NULL: si la file est vide
+        - Node *: pointeur vers la queue de la file
+***********************************************************************************************************************/
+Node *file_queue_file(Node *queue);
+
+/***********************************************************************************************************************
+    INPUT:
+        - tete_ptr: pointeur vers la tête de la file
+        - queue_ptr: pointeur vers la queue de la file
+    PROCESS:
+        retire l'élément en tête de la file
+    OUTPUT:
+        - NULL: si la file est vide
+        - Node *: pointeur vers la tête de la file
+***********************************************************************************************************************/
+Node *file_defiler(Node **tete_ptr, Node **queue_ptr);
 
 #endif //ALGORITHMIQUE_FILE_H
