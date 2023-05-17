570b55ed633bd6a2e800b4a2afd5f24c084e9540
TMH <copusestan@gmail.com>
2023-05-17 16:00:19 +0200

    add list functions

--- Structures de données dynamiques/Listes/Liste linéraire unidirectionnelle/llu.c
+++ Structures de données dynamiques/Listes/Liste linéraire unidirectionnelle/llu.c
@@ -1,1 +1,109 @@
 #include "llu.h"
+
+/***********************************************************************************************************************
+    Voir llu.h pour la documentation
+***********************************************************************************************************************/
+
+int liste_est_vide(Node *liste)
+{
+    return liste == NULL; // retourne 1 si la liste est vide, 0 sinon
+}
+
+int liste_insertion_triee(Node **liste_ptr, Node *element)
+{
+    Node *new = NULL; // pointeur vers le nouvel élément
+    Node *cur = NULL; // pointeur vers l'élément courant
+    Node *pre = NULL; // pointeur vers l'élément précédent
+
+    new = (Node *) malloc(sizeof(Node)); // allocation de la mémoire pour le nouvel élément
+
+    if (new == NULL) // si l'allocation a échoué
+        return 0;
+
+    // copie des données de l'élément à insérer dans le nouvel élément
+    strcpy(new->nom, element->nom);
+    new->age = element->age;
+
+    // insertion du nouvel élément dans la liste
+
+    // si la liste est vide ou si le nouvel élément doit être inséré en tête de liste
+    if (*liste_ptr == NULL || strcmp(new->nom, (*liste_ptr)->nom) < 0 ||
+        (strcmp(new->nom, (*liste_ptr)->nom) == 0 && new->age < (*liste_ptr)->age))
+
+    {
+        new->next = *liste_ptr; // le champ next du nouvel élément pointe vers le premier élément de la liste
+        *liste_ptr = new; // le pointeur vers la liste pointe vers le nouvel élément
+    }
+    else // si le nouvel élément doit être inséré ailleurs dans la liste
+    {
+        cur = *liste_ptr; // le pointeur courant pointe vers le premier élément de la liste
+
+        // Cherche l'élément après lequel insérer le nouvel élément
+        while (cur != NULL && strcmp(new->nom, cur->nom) > 0)
+        {
+            pre = cur; // le pointeur précédent pointe vers l'élément courant
+            cur = cur->next; // le pointeur courant pointe vers l'élément suivant
+        }
+        while (cur != NULL && strcmp(new->nom, cur->nom) == 0 && new->age >= cur->age)
+        {
+            pre = cur;
+            cur = cur->next;
+        }
+        new->next = cur; // le champ next du nouvel élément pointe vers l'élément suivant
+        pre->next = new; // le champ next de l'élément précédent pointe vers le nouvel élément
+    }
+    return 1; // retourne 1 si l'élément a été inséré dans la liste
+}
+
+Node *liste_recherche(Node *liste, char *nom_element)
+{
+    // Parcours de la liste jusqu'à trouver l'élément ou jusqu'à la fin de la liste
+    while (liste != NULL && strcmp(nom_element, liste->nom) > 0)
+        liste = liste->next;
+
+    // Si l'élément a été trouvé, retourne un pointeur vers l'élément
+    if (liste != NULL && strcmp(nom_element, liste->nom) == 0)
+        return liste;
+
+    return NULL; // sinon retourne NULL
+}
+
+void liste_suppression(Node **liste_ptr, char *nom_element)
+{
+    Node *cur = NULL; // pointeur vers l'élément courant
+    Node *pre = NULL; // pointeur vers l'élément précédent
+
+    // si la liste n'est pas vide
+    if(*liste_ptr != NULL)
+    {
+        cur = *liste_ptr; // le pointeur courant pointe vers le premier élément de la liste
+
+        // Cherche l'élément à supprimer
+        while (cur != NULL && strcmp(nom_element, cur->nom) > 0)
+        {
+            pre = cur; // le pointeur précédent pointe vers l'élément courant
+            cur = cur->next; // le pointeur courant pointe vers l'élément suivant
+        }
+        // si l'élément a été trouvé
+        if (cur != NULL && strcmp(nom_element, cur->nom) == 0)
+        {
+            // si l'élément à supprimer est en tête de liste
+            if (pre == NULL)
+                *liste_ptr = cur->next; // le pointeur vers la liste pointe vers l'élément suivant
+            else
+                pre->next = cur->next; // le champ next de l'élément précédent pointe vers l'élément suivant
+
+            free(cur); // libère la mémoire occupée par l'élément à supprimer
+        }
+    }
+}
+
+void liste_affichage(Node *liste)
+{
+    while (liste != NULL)
+    {
+        printf("%s\n", liste->nom);
+        printf("%d\n", liste->age);
+        liste = liste->next;
+    }
+}
--- Structures de données dynamiques/Listes/Liste linéraire unidirectionnelle/llu.h
+++ Structures de données dynamiques/Listes/Liste linéraire unidirectionnelle/llu.h
@@ -3,4 +3,84 @@
 
 #include "..\..\..\data.h"
 
+/***********************************************************************************************************************
+
+    Une liste linéaire est la représentation informatique d’un ensemble fini et ordonné
+    d’éléments de même type.
+
+    On dit que la liste est linéaire unidirectionnelle si chaque enregistrement est capable de
+    donner l'emplacement de l'enregistrement suivant ce qui constitue un chaînage. Pour connaître une
+    telle liste, il suffira de connaître l'emplacement de son premier enregistrement, inversement, elle
+    ne pourra pas être connue si on ne connaît pas celui-ci. On notera en abrégé L.L.U.
+
+    Pour créer une liste il suffit de mettre le pointeur qui la représente à NULL. Elle est vide.
+
+    Les opérations principales que l'on peut effectuer sur une liste linéaire sont :
+        - Test de liste vide
+        - Insertion d'un élément dans une liste linéaire
+        - Recherche d'un élément dans une liste linéaire
+        - Suppression d'un élément dans une liste linéaire
+        - Affichage d'une liste linéaire
+
+    La liste est considérée comme triée sur le nom puis sur l'âge.
+
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+    INPUT:
+        - liste: pointeur vers la liste
+    PROCESS:
+        teste si la liste est vide
+    OUTPUT:
+        - 1: si la liste est vide
+        - 0: si la liste n'est pas vide
+***********************************************************************************************************************/
+int liste_est_vide(Node *liste);
+
+/***********************************************************************************************************************
+    INPUT:
+        - liste_ptr: pointeur vers la liste
+        - element: pointeur vers l'élément à insérer
+    PROCESS:
+        insère un élément dans une liste triée
+    OUTPUT:
+        - 1: si l'élément a été inséré dans la liste
+        - 0: si l'élément n'a pas été inséré dans la liste
+***********************************************************************************************************************/
+int liste_insertion_triee(Node **liste_ptr, Node *element);
+
+/***********************************************************************************************************************
+    INPUT:
+        - liste: pointeur vers la liste
+        - nom_element: nom de l'élément à rechercher
+    PROCESS:
+        recherche un élément dans une liste triée
+    OUTPUT:
+        - pointeur vers l'élément recherché si l'élément est trouvé
+        - NULL si l'élément n'est pas trouvé
+***********************************************************************************************************************/
+Node * liste_recherche(Node *liste, char *nom_element);
+
+
+/***********************************************************************************************************************
+    INPUT:
+        - liste_ptr: pointeur vers la liste
+        - nom_element: nom de l'élément à supprimer
+    PROCESS:
+        supprime un élément dans une liste triée
+    OUTPUT:
+        - aucune
+***********************************************************************************************************************/
+void liste_suppression(Node **liste_ptr, char *nom_element);
+
+/***********************************************************************************************************************
+    INPUT:
+        - liste: pointeur vers la liste
+    PROCESS:
+        affiche une liste
+    OUTPUT:
+        - aucune
+***********************************************************************************************************************/
+void liste_affichage(Node *liste);
+
 #endif //ALGORITHMIQUE_LLU_H
