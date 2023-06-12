cd7d4a463765e85ade84997366f01c0628b326c5
TMH <copusestan@gmail.com>
2023-06-12 15:21:22 +0200

    fix j starting value and indes linked to it

--- Algorithmes de tri/Tris simples/Tri par extraction/algo_tri_extract.c
+++ Algorithmes de tri/Tris simples/Tri par extraction/algo_tri_extract.c
@@ -4,12 +4,12 @@ void algo_tri_extract(int tab[], int taille_tab)
 {
     int i, j, indice_max, max;
 
-    for (j = taille_tab; j > 0; j--)
+    for (j = taille_tab - 1; j > 0; j--)
     {
         max = tab[0]; // on suppose que le maximum est le premier élément
         indice_max = 0;
 
-        for (i = 1; i < j; i++)
+        for (i = 1; i <= j; i++)
         {
             if (tab[i] > max) // si l'élément courant est supérieur au maximum
             {
@@ -18,7 +18,7 @@ void algo_tri_extract(int tab[], int taille_tab)
             }
         }
 
-        tab[indice_max] = tab[j - 1]; // on échange le maximum avec le dernier élément
-        tab[j - 1] = max;
+        tab[indice_max] = tab[j]; // on échange le maximum avec le dernier élément
+        tab[j] = max;
     }
 }
