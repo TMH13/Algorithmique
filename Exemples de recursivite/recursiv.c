c950305580a1d9ec6e9908883dca678f19261b01
TMH <39454976+TMH13@users.noreply.github.com>
2023-07-01 17:50:08 +0200

    simplify the recursive example

--- Exemples de recursivite/recursiv.c
+++ Exemples de recursivite/recursiv.c
@@ -4,6 +4,5 @@ int recursivite_factorielle(int n)
 {
     if (n == 0)
         return 1;
-    else
-        return n * recursivite_factorielle(n - 1);
+    return n * recursivite_factorielle(n - 1);
 }
